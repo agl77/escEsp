0804f8a229f343953d63f6f1b605cc03faa0d6a9
agl77 <agl1977@gmail.com>
2023-11-14 07:40:33 -0300

    Valres salvando em bd

---
+++ html/processa_valores.php
@@ -0,0 +1,29 @@
+<?php
+// Configurações de conexão com o MySQL
+include ('db_conf.php');
+
+try {
+   // Conecta ao banco de dados usando PDO
+   $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+   $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+   // Dados do formulário
+   $idcadastro = $_POST['idcadastro']; // Certifique-se de validar/sanitizar isso conforme necessário
+   $selectedValues = $_POST['values'];
+   
+   // Converte os arrays para strings sem quebras de linha e espaços adicionais
+   $liked = str_replace(["\n", "\r", "  "], "", implode(', ', json_decode($_POST['liked'])));
+   $disliked = str_replace(["\n", "\r", "  "], "", implode(', ', json_decode($_POST['disliked'])));
+
+   //Insere os dados na tabela 'cadastro'
+   $sql = "INSERT INTO esp_valor (idcadastro, liked, disliked, selectedValues) VALUES (?, ?, ?, ?)";
+   $stmt = $pdo->prepare($sql);
+   $stmt->execute([$idcadastro, $liked, $disliked, implode(', ', $selectedValues)]);
+
+   //Redireciona para a próxima página
+   header("Location: paginado.php");
+
+} catch (PDOException $e) {
+   echo "Erro ao inserir os dados: " . $e->getMessage();
+}
+?>
--- html/processar.php
+++ html/processar.php
@@ -1,43 +1,44 @@
 <?php
 // Configurações de conexão com o MySQL
-// $host = 'mysql';
-// $usuario = 'base';
-// $senha = 'abc';
-// $banco = 'base';
-// header("Location: esp.php");
+include ('db_conf.php');
 
-// try {
-//    // Conecta ao banco de dados usando PDO
-//    $pdo = new PDO("mysql:host=$host;dbname=$banco", $usuario, $senha);
-//   $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-//     Dados do formulário
-//    $nome = $_POST['nome'];
-//    $nascimento = $_POST['data_nascimento'];
-//    $instituicao = $_POST['instituicao'];
-//    $formado = isset($_POST['formado']) ? 1 : 0;
-//    $periodo = $formado ? $_POST['ano_conclusao'] : $_POST['semestre'];
-//    $especialidade = $formado ? $_POST['especialidade'] : null;
+ try {
+   // Conecta ao banco de dados usando PDO
+   $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+   $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-//     Insere os dados na tabela 'cadastro'
-//    $sql = "INSERT INTO cadastro (nome, nascimento, formado, instituicao, periodo, especialidade) VALUES (?, ?, ?, ?, ?, ?)";
-//    $stmt = $pdo->prepare($sql);
-//    $stmt->execute([$nome, $nascimento, $formado, $instituicao, $periodo, $especialidade]);
+   // Dados do formulário
+   $nome = $_POST['nome'];
+   $nascimento = $_POST['data_nascimento'];
+   $cpf = $_POST['cpf'];
+   $email = $_POST['email'];
+   $telefone = $_POST['telefone'];
+   $instituicao = $_POST['instituicao'];
+   $formado = isset($_POST['formado']) ? 1 : 0;
+   $periodo = $formado ? $_POST['ano_conclusao'] : $_POST['semestre'];
+   $especialidade = $formado ? $_POST['especialidade'] : null;
+   $aceitarTermos = isset($_POST['aceitar_termos']) ? 1 : 0;
 
-//     ID do cadastro inserido
-//    $idcadastro = $pdo->lastInsertId();
-//    //insere no localstorage o idcadastro
-//    echo '<script>localStorage.setItem("idcadastro", ' . $idcadastro . ')</script>';
-//    //zera o localstorage
-//    echo '<script>localStorage.clear();</script>';
+   //Insere os dados na tabela 'cadastro'
+   $sql = "INSERT INTO cadastro (nome, nascimento, CPF, email, telefone, instituicao, formado, periodo, especialidade, aceite) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+   $stmt = $pdo->prepare($sql);
+   $stmt->execute([$nome, $nascimento, $cpf, $email, $telefone, $instituicao, $formado, $periodo, $especialidade, $aceitarTermos]);
 
-//    //Redireciona para a próxima página com o ID do cadastro
-    header("Location: esp.php");
- 
+    //zera o localstorage
+    echo '<script>localStorage.clear();</script>';
 
-// } catch (PDOException $e) {
-//    echo "Erro ao inserir os dados: " . $e->getMessage();
-// }
+   // ID do cadastro inserido
+   $idcadastro = $pdo->lastInsertId();
+   //insere no localstorage o idcadastro
+   echo '<script>localStorage.setItem("idcadastro", ' . $idcadastro . ')</script>';
+   //Redireciona para a próxima página com o ID do cadastro
+   header("Location: esp.php");
+
+
+} catch (PDOException $e) {
+   echo "Erro ao inserir os dados: " . $e->getMessage();
+}
 
 // Fecha a conexão com o banco de dados (o PDO faz isso automaticamente)
 ?>
